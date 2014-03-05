c50ad8aa2e69f61d4bf339e6828f307f5fa82df2
hoosin <hoosin@yeah.net>
2014-03-05 18:27:22 +0800

    update

--- fun/js/js.js
+++ fun/js/js.js
@@ -1,3 +1,9 @@
+ 
 <!--#include virtual="../lib/jquery-2.1.0.js"-->
-<!--#include virtual="../lib/jquery.lazyLoad.js"-->
 <!--#include virtual="../lib/returnTop.js"-->
+
+// 按需加载
+(function(){
+    <!--#include virtual="../lib/jquery.lazyLoad.js"-->
+    $("#list li img.J_lazyLoad").scrollLoading({attr:"data-original"});
+})();
