a19a434a54643df86a6be8dea80be12228d85b23
hoosin <hoosin@yeah.net>
2014-03-06 10:10:26 +0800

    add

--- fun/js/js.js
+++ fun/js/js.js
@@ -1,9 +1,19 @@
  
 <!--#include virtual="../lib/jquery-2.1.0.js"-->
-<!--#include virtual="../lib/returnTop.js"-->
 
 // 按需加载
-(function(){
-    <!--#include virtual="../lib/jquery.lazyLoad.js"-->
-    $("#list li img.J_lazyLoad").scrollLoading({attr:"data-original"});
-})();
+
+
+var html='<li><img class="J_lazyLoad" data-original="http://placekitten.com/300/400" src="https://raw.github.com/hoosin/lite/master/fun/img/loading.gif"></li>'
+var list=document.getElementById('list');
+list.innerHTML=html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html+html;
+
+window.onload = function() {
+	(function() {
+		<!--#include virtual="../lib/jquery.lazyLoad.js"-->
+		$("#list li img.J_lazyLoad").scrollLoading({
+			init: false,
+			attr: "data-original"
+		});
+	})();
+};
